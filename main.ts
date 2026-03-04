a8b1081d96b1cbfa91d0103064a65ec1133be3fb
TK Yosh <2903380+ystakebe@users.noreply.github.com>
2026-03-04 18:42:43 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-	
-})
 /**
  * サイドチェンジ
  * 
  * 　LED
  */
+function check_count () {
+    if (true) {
+    	
+    }
+}
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+	
+})
 input.onButtonPressed(Button.A, function () {
     if (side > 0) {
         if (count < count_max) {
             count += 1
             count_up += 1
+            check_count()
         }
     }
 })
@@ -22,11 +28,15 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     }
     side = side * -1
 })
+function reset_game () {
+    count_up = 0
+    count_max = 3
+}
+let count_up = 0
+let count_max = 0
 let count = 0
 let side = 0
-let count_max = 0
-let count_up = 0
-count_max = 3
+reset_game()
 basic.forever(function () {
-    basic.showNumber(count_up)
+    led.plot(0, 0)
 })
