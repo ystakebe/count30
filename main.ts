dcad7c2f1122bdad3896524cf9c6841ecfe85356
TK Yosh <2903380+ystakebe@users.noreply.github.com>
2026-03-04 19:49:50 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,89 @@
-/**
- * サイドチェンジ
- * 
- * 　LED
- */
-function check_count () {
-    if (true) {
-    	
+function count_up2 () {
+    if (count < count_max) {
+        count += 1
+        count_up += 1
+        if (count_up == 20) {
+            finishgame()
+        }
+        if (count == count_max) {
+            sidechange()
+        }
     }
 }
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-	
+    reset_game()
 })
+function sidechange () {
+    basic.clearScreen()
+    side = side * -1
+    if (side > 0) {
+        basic.showArrow(ArrowNames.West)
+    } else {
+        basic.showArrow(ArrowNames.East)
+    }
+    count = 0
+    basic.clearScreen()
+}
 input.onButtonPressed(Button.A, function () {
     if (side > 0) {
-        if (count < count_max) {
-            count += 1
-            count_up += 1
-            check_count()
+        count_up2()
+        showcount()
+    }
+})
+function showcount () {
+    led_x = (count_up - count_up % 5) / 5
+    led_y = count_up % 5
+    if (led_x > 0) {
+        for (let カウンター = 0; カウンター <= led_x - 1; カウンター++) {
+            led.plot(カウンター, 0)
+            led.plot(カウンター, 1)
+            led.plot(カウンター, 2)
+            led.plot(カウンター, 3)
+            led.plot(カウンター, 4)
         }
+    }
+    if (led_y > 0) {
+        for (let カウンター = 0; カウンター <= led_y - 1; カウンター++) {
+            led.plot(led_x, カウンター)
+        }
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    if (side < 0) {
+        count_up2()
+        showcount()
     }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (side > 0) {
-    	
-    } else {
-    	
-    }
-    side = side * -1
+    sidechange()
+    showcount()
 })
 function reset_game () {
     count_up = 0
-    count_max = 3
+    side = randint(0, 1)
+    if (Math.randomBoolean()) {
+        side = 1
+        basic.showArrow(ArrowNames.West)
+    } else {
+        side = -1
+        basic.showArrow(ArrowNames.East)
+    }
 }
-let count_up = 0
-let count_max = 0
-let count = 0
+function finishgame () {
+    basic.clearScreen()
+    if (side > 0) {
+        basic.showString("B Win!")
+    } else {
+        basic.showString("A Win!")
+    }
+    basic.clearScreen()
+}
+let led_y = 0
+let led_x = 0
 let side = 0
+let count_up = 0
+let count = 0
+let count_max = 0
+count_max = 3
 reset_game()
-basic.forever(function () {
-    led.plot(0, 0)
-})
+basic.clearScreen()
